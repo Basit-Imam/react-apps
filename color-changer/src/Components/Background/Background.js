ce0afa4736e644a0dc7c1b357c15f5187367ab2a
Basit <basitimam29@gmail.com>
2020-08-28 17:01:13 +0530

    files added

---
+++ color-changer/src/Components/Background/Background.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './style.css'
+
+class Background extends React.Component{
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedColor:"White"
+        }
+    }
+
+    changeBackgroundColor = (event) => {
+        console.log("color changed to ", event.target.value)
+
+        this.setState({
+            selectedColor:event.target.value
+        })
+    }
+
+    // @override
+    render = () => {
+
+        return (
+            <div style={{
+                backgroundColor: this.state.selectedColor
+            }}className='back-container'>
+                <div className='color-box'>
+                    <h2>Choose a color.</h2>
+                    <input onChange={this.changeBackgroundColor} type='color'/>
+                    <p>{this.state.selectedColor}</p>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+
+export default Background
